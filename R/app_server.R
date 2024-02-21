dacf86b3746917b190d7e4299fc51f52ee38ee2c
FrancescaMancini <framan@ceh.ac.uk>
2024-02-21 13:47:31 +0000

    Set up

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,90 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  mod_info_tab_server("info_tab_1")
+
+  # upload a data file
+  # can only be .csv or .txt
+  # otherwise return message "Invalid file"
+
+  # uploaded_data <- reactiveVal()
+
+ uploaded_data <- reactive({
+   req(input$upload)
+
+    ext <- tools::file_ext(input$upload$name)
+    switch(ext,
+           csv = vroom::vroom(input$upload$datapath, delim = ","),
+           txt = vroom::vroom(input$upload$datapath, delim = "\t"),
+           validate("Invalid file; Please upload a .csv or .txt file")
+    )
+  })
+
+  # when checkbox for converting grid references is ticked
+  # a drop down menu appears to select the gridref column
+  #  and an action button to convert them to lat and long
+
+  observeEvent(input$grid_ref, {
+    insertUI(
+      selector = paste0("#", "placeholder"),
+      where = "beforeEnd",
+      ui =  fluidRow(
+            tagList(
+          varSelectInput(
+            "grid_ref_column", "Grid Reference column",
+            data = uploaded_data()
+          ),
+          actionButton(
+            "grid_ref_convert",
+            "Convert")
+        )
+      )
+    )
+  })
+
+  # when the action button is clicked convert the
+  # grid references into lat and long
+  lat_lon <- eventReactive(input$grid_ref_convert, {
+    req(input$grid_ref_column)
+
+    sites <- pull(uploaded_data(), eval(as.symbol(input$grid_ref_column)))
+    osg_parse(grid_refs = sites, coord_system = "WGS84")
+  })
+
+  # this should work but it doesn't
+
+  # observeEvent(input$grid_ref_convert, {
+  #   req(uploaded_data(), lat_lon(), input$grid_ref_column)
+  #   # if ('data.frame' %in% class(uploaded_data())){
+  #     uploaded_data()$lat <- lat_lon()$lat
+  #     uploaded_data()$lon <- lat_lon()$lon
+  #   # }
+  # })
+  #
+
+
+  # create the datatable only when the view data button is clicked
+
+  view_table <- eventReactive(input$view_button, {
+    req(input$view_button)
+
+    datatable(uploaded_data())
+  })
+
+  # render the table
+  output$data_table <-DT::renderDT({
+    view_table()
+  })
+
+
+
+  mod_data_tab_server("data_tab_1", uploaded_data = uploaded_data)
+  mod_time_bias_tab_server("time_bias_tab_1", uploaded_data = uploaded_data)
+  mod_species_bias_tab_server("species_bias_tab_1")
+  mod_species_id_bias_tab_server("species_id_bias_tab_1")
+  mod_rarity_bias_tab_server("rarity_bias_tab_1")
+  mod_space_cov_tab_server("space_cov_tab_1")
+  mod_space_bias_tab_server("space_bias_tab_1")
+  mod_environment_bias_tab_server("environment_bias_tab_1")
+  mod_export_tab_server("export_tab_1")
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -4,13 +4,96 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
+
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
-    fluidPage(
-      h1("dataxplore")
+    navbarPage(
+      "dataXplore",
+      tabPanel("Info",
+               mod_info_tab_ui("info_tab_1")
+      ),
+      tabPanel("Data",
+               fluidPage(theme = bslib::bs_theme(bootswatch = "sandstone"),
+                 sidebarLayout(
+                   sidebarPanel(
+                     fileInput("upload",
+                               "Upload your data",
+                               accept=c(".csv", ".txt")),
+                     checkboxInput(
+                       "grid_ref",
+                       "Convert British National Grid References", FALSE),
+                     tags$div(id = 'placeholder'),
+                     actionButton(
+                       "view_button", "View data")),
+                   mainPanel(
+                     DTOutput("data_table")
+                     )
+                 ),
+                 mod_data_tab_ui("data_tab_1")
+               )
+      ),
+      tabPanel("Time",
+               fluidPage(
+                 theme = bslib::bs_theme(bootswatch = "sandstone"),
+                 mod_time_bias_tab_ui("time_bias_tab_1")
+               )
+      ),
+      tabPanel("Species",
+               tabsetPanel(
+                 tabPanel("Species number",
+                          fluidPage(theme = bslib::bs_theme(bootswatch = "sandstone"),
+                                    mod_species_bias_tab_ui("species_bias_tab_1")
+                          )
+                 ),
+                 tabPanel("Species ID",
+                          fluidPage(theme = bslib::bs_theme(bootswatch = "sandstone"),
+                                    mod_species_id_bias_tab_ui("species_id_bias_tab_1")
+
+                          )
+                 ),
+                 tabPanel("Rarity bias",
+                          fluidPage(theme = bslib::bs_theme(bootswatch = "sandstone"),
+                                    mod_rarity_bias_tab_ui("rarity_bias_tab_1")
+                          )
+                 )
+               ) #Close inner tabsetPanel
+      ),
+      tabPanel("Space",
+               tabsetPanel(
+                 tabPanel("Spatial coverage",
+                          fluidPage(theme = bslib::bs_theme(bootswatch = "sandstone"),
+                                    mod_space_cov_tab_ui("space_cov_tab_1")
+                          )
+                 ),
+                 tabPanel("Spatial bias",
+                          fluidPage(theme = bslib::bs_theme(bootswatch = "sandstone"),
+                                    mod_space_bias_tab_ui("space_bias_tab_1")
+
+                          )
+                 )
+               ) #Close inner tabsetPanel
+      ),
+      # tabPanel("Space",
+      #          fluidPage(
+      #            theme = bslib::bs_theme(bootswatch = "sandstone"),
+      #            mod_space_bias_tab_ui("space_bias_tab_1")
+      #          )
+      # ),
+      tabPanel("Environment",
+               fluidPage(
+                 theme = bslib::bs_theme(bootswatch = "sandstone"),
+                 mod_environment_bias_tab_ui("environment_bias_tab_1")
+               )
+      ),
+      tabPanel("Export",
+               fluidPage(
+                 theme = bslib::bs_theme(bootswatch = "sandstone"),
+                 mod_export_tab_ui("export_tab_1")
+               )
+      )
     )
   )
 }
