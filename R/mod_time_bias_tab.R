c60999cbc6565109f3923d6782f26bc2767e2da5
DylanCarbone <dylcar@ceh.ac.uk>
2024-03-05 16:24:12 +0000

    Added ability to group records into year periods for plotting

--- R/mod_time_bias_tab.R
+++ R/mod_time_bias_tab.R
@@ -11,7 +11,7 @@
 #' @importFrom bslib tooltip
 #' @importFrom bsicons bs_icon
 #' @importFrom shinyWidgets numericRangeInput
-mod_time_bias_tab_ui <- function(id){
+mod_time_bias_tab_ui <- function(id) {
   ns <- NS(id)
   tagList(
     sidebarLayout(
@@ -26,14 +26,12 @@ mod_time_bias_tab_ui <- function(id){
         ),
         radioButtons(
           ns("periodtype"), "Time periods as",
-          choiceNames = list("Years","Year ranges"),
-          choiceValues = list("years","ranges"),
+          choiceNames = list("Years", "Year ranges"),
+          choiceValues = list("years", "ranges"),
           selected = character(0)
         ),
-        uiOutput(ns("numUI")
-        ),
-        uiOutput(ns("dateRangesUI")
-        ),
+        uiOutput(ns("numUI")),
+        uiOutput(ns("dateRangesUI")),
         # varSelectInput(
         #   ns("species"), "Species column",
         #   data = NULL
@@ -71,8 +69,7 @@ mod_time_bias_tab_ui <- function(id){
         ),
         # checkboxInput("code", "View R code"
         # ),
-        checkboxInput("report", "Add to report", FALSE
-        )
+        checkboxInput("report", "Add to report", FALSE)
       ),
       mainPanel(
         h2(
@@ -83,7 +80,8 @@ mod_time_bias_tab_ui <- function(id){
               "The plot displays the number of records in each time period.",
               placement = "bottom"
             )
-          )),
+          )
+        ),
         plotOutput(ns("number_records"))
       )
     )
@@ -93,30 +91,40 @@ mod_time_bias_tab_ui <- function(id){
 #' time_bias_tab Server Functions
 #'
 #' @noRd
-mod_time_bias_tab_server <- function(id, uploaded_data){
-  moduleServer( id, function(input, output, session){
+mod_time_bias_tab_server <- function(id, uploaded_data) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
     observeEvent(uploaded_data(), {
-      updateSelectInput(session, "year", choices = names(uploaded_data()),
-                        selected=character(0))
-      updateSelectInput(session, "species", choices = names(uploaded_data()),
-                           selected=character(0))
-      updateSelectInput(session, "lon", choices = names(uploaded_data()),
-                           selected=character(0))
-      updateSelectInput(session, "lat", choices = names(uploaded_data()),
-                           selected=character(0))
-      updateSelectInput(session, "ident", choices = names(uploaded_data()),
-                           selected=character(0))
+      updateSelectInput(session, "year",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+      updateSelectInput(session, "species",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+      updateSelectInput(session, "lon",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+      updateSelectInput(session, "lat",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+      updateSelectInput(session, "ident",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
     })
 
 
     output$numUI <- renderUI({
       req(input$periodtype == "ranges")
-        numericInput(
-          ns("num"),"Time periods",
-          value = 1, min = 1, max = Inf
-          )
+      numericInput(
+        ns("num"), "Time periods",
+        value = 1, min = 1, max = Inf
+      )
     })
 
     output$dateRangesUI <- renderUI({
@@ -124,40 +132,157 @@ mod_time_bias_tab_server <- function(id, uploaded_data){
       num <- input$num
       dateRanges <- lapply(1:num, function(i) {
         numericRangeInput(paste0("dates_", i),
-                          label = paste("Year range", i),
-                          value = c(uploaded_data() %>%
-                                      select(input$year) %>%
-                                      summarise(min = min(eval(as.name(input$year)))) %>%
-                                      pull(),
-                                    uploaded_data() %>%
-                                      select(input$year) %>%
-                                      summarise(max = max(eval(as.name(input$year)))) %>%
-                                      pull()))
+          label = paste("Year range", i),
+          value = c(
+            uploaded_data() %>%
+              select(input$year) %>%
+              summarise(min = min(eval(as.name(input$year)))) %>%
+              pull(),
+            uploaded_data() %>%
+              select(input$year) %>%
+              summarise(max = max(eval(as.name(input$year)))) %>%
+              pull()
+          )
+        )
       })
       tagList(dateRanges)
     })
 
     observeEvent(input$plot_button, {
-
       req(input$species, input$year, input$lon, input$lat, input$ident, uploaded_data())
-      
-      dat <- as.data.frame(uploaded_data())
-      
-      output$number_records <- renderPlot({
-        # Your plotting code here
-        assessRecordNumber(dat = dat,
-                          species = input$species,
-                          periods = unique(dat[, input$year]),
-                          x = input$lon,
-                          y = input$lat,
-                          year = input$year,
-                          spatialUncertainty = NULL,
-                          identifier = input$ident)$plot
-  })
-})
 
-   })
+      dat <- as.data.frame(uploaded_data())
+
+      if (input$periodtype == "ranges") {
+        ranges_input_names <- paste0("dates_", 1:input$num)
+
+        # Step 1: Filter the list based on 'period_ranges'
+        year_ranges <- input[names(input) %in% ranges_input_names]
+
+        # Step 2: Remove the names from the filtered list
+        names(year_ranges) <- NULL
+
+        # Convert the year_ranges into vectors with year intervals of 1
+        periods <- lapply(filtered_list, FUN = function(element) {
+          return(seq(from = element[1], to = element[2]))
+        })
+      } else {
+        periods <- unique(dat[, input$year])
+      }
+
+      output$number_records <- renderPlot({
+        assessRecordNumber(
+          dat = dat,
+          species = input$species,
+          periods = periods,
+          x = input$lon,
+          y = input$lat,
+          year = input$year,
+          spatialUncertainty = NULL,
+          identifier = input$ident
+        )$plot
+      })
+    })
+  })
 }
+
+mod_time_bias_tab_server <- function(id, uploaded_data) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    observeEvent(uploaded_data(), {
+      updateSelectInput(session, "year",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+      updateSelectInput(session, "species",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+      updateSelectInput(session, "lon",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+      updateSelectInput(session, "lat",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+      updateSelectInput(session, "ident",
+        choices = names(uploaded_data()),
+        selected = character(0)
+      )
+    })
+
+    output$numUI <- renderUI({
+      req(input$periodtype == "ranges")
+      numericInput(
+        ns("num"), "Time periods",
+        value = 1, min = 1, max = Inf
+      )
+    })
+
+    output$dateRangesUI <- renderUI({
+      req(input$periodtype == "ranges", input$num)
+      num <- input$num
+      dateRanges <- lapply(1:num, function(i) {
+        numericRangeInput(ns(paste0("dates_", i)),
+          label = paste("Year range", i),
+          value = c(
+            uploaded_data() %>%
+              select(input$year) %>%
+              summarise(min = min(eval(as.name(input$year)))) %>%
+              pull(),
+            uploaded_data() %>%
+              select(input$year) %>%
+              summarise(max = max(eval(as.name(input$year)))) %>%
+              pull()
+          )
+        )
+      })
+      tagList(dateRanges)
+    })
+
+    observeEvent(input$plot_button, {
+      req(input$species, input$year, input$lon, input$lat, input$ident, uploaded_data())
+
+      dat <- as.data.frame(uploaded_data())
+
+      if (input$periodtype == "ranges") {
+        ranges_input_names <- paste0("dates_", 1:input$num)
+
+        # Filter the list based on 'period_ranges'
+        year_ranges <- lapply(ranges_input_names, function(x) input[[x]])
+
+        # Convert the year_ranges into vectors with year intervals of 1
+        periods <- lapply(year_ranges, function(element) {
+          seq(from = element[1], to = element[2])
+        })
+
+        if (length(periods) == 1){
+
+          periods = unlist(periods)
+        }
+
+      } else {
+        periods <- unique(dat[[input$year]])
+      }
+
+      output$number_records <- renderPlot({
+        assessRecordNumber(
+          dat = dat,
+          species = input$species,
+          periods = periods,
+          x = input$lon,
+          y = input$lat,
+          year = input$year,
+          spatialUncertainty = NULL,
+          identifier = input$ident
+        )$plot
+      })
+    })
+  })
+}
+
 
 ## To be copied in the UI
 #
