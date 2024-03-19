bc9c6171f475acaddcc48c5d3236f92a3c4aff65
FrancescaMancini <framan@ceh.ac.uk>
2024-03-19 12:41:22 +0000

    Update UI and server function for all species bias tabs

--- R/mod_species_id_bias_tab.R
+++ R/mod_species_id_bias_tab.R
@@ -12,39 +12,52 @@ mod_species_id_bias_tab_ui <- function(id){
   tagList(
     sidebarLayout(
       sidebarPanel(
-        numericInput(ns("num"),
-                     "Time periods",
-                     value = 1, min = 1, max = Inf
+        selectInput(
+          ns("year"), "Year column",
+          choices = NULL
         ),
-        uiOutput(ns("dateRangesUI")
+        radioButtons(
+          ns("periodtype"), "Time periods as",
+          choiceNames = list("Years", "Year ranges"),
+          choiceValues = list("years", "ranges"),
+          selected = character(0)
+        ),
+        uiOutput(ns("numUI")),
+        uiOutput(ns("dateRangesUI")),
+        selectInput(
+          ns("species"), "Species column",
+          choices = NULL
         ),
         selectInput(
-          "species", "Species column",
-          c("", "species", "date", "x", "y", "group", "year"),
+          ns("lon"), "Longitude column",
+          choices = NULL
         ),
         selectInput(
-          "lon", "Longitude column",
-          c("", "species", "date", "x", "y", "group", "year"),
+          ns("lat"), "Latitude column",
+          choices = NULL
         ),
         selectInput(
-          "lat", "Latitude column",
-          c("", "species", "date", "x", "y", "group", "year"),
+          ns("spat_uncert"), "Spatial Uncertainty column",
+          choices = NULL
+        ),
+        numericInput(
+          ns("max_spat_uncert"), "Maximum Spatial Uncertainty",
+          value = 10000,
         ),
         selectInput(
-          "year", "Year column",
-          c("", "species", "date", "x", "y", "group", "year"),
+          ns("ident"), "Choose the identifier",
+          choices = NULL
         ),
         selectInput(
-          "id", "Choose the identifier",
-          c("", "species", "date", "x", "y", "group", "year"),
+          ns("type"), "Type",
+          choices = c("count", "proportion")
         ),
         actionButton(
-          "plot_button", "Plot"
+          ns("plot_button"), "Plot"
         ),
         # checkboxInput("code", "View R code"
         # ),
-        checkboxInput("report", "Add to report", FALSE
-        )
+        checkboxInput("report", "Add to report", FALSE)
       ),
       mainPanel(
         h2(
@@ -68,22 +81,126 @@ mod_species_id_bias_tab_ui <- function(id){
 #' species_id_bias_tab Server Functions
 #'
 #' @noRd
-mod_species_id_bias_tab_server <- function(id){
+mod_species_id_bias_tab_server <- function(id, uploaded_data){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
+    observeEvent(uploaded_data(), {
+      updateSelectInput(session, "year",
+                        choices = names(uploaded_data()),
+                        selected = character(0)
+      )
+      updateSelectInput(session, "species",
+                        choices = names(uploaded_data()),
+                        selected = character(0)
+      )
+      updateSelectInput(session, "lon",
+                        choices = names(uploaded_data()),
+                        selected = character(0)
+      )
+      updateSelectInput(session, "lat",
+                        choices = names(uploaded_data()),
+                        selected = character(0)
+      )
+      updateSelectInput(session, "ident",
+                        choices = names(uploaded_data()),
+                        selected = character(0)
+      )
+      updateSelectInput(session, "spat_uncert",
+                        choices = names(uploaded_data()),
+                        selected = character(0)
+      )
+
+    })
+
+
+    output$numUI <- renderUI({
+      req(input$periodtype == "ranges")
+      numericInput(
+        ns("num"), "Time periods",
+        value = 1, min = 1, max = Inf
+      )
+    })
+
     output$dateRangesUI <- renderUI({
+      req(input$periodtype == "ranges", input$num)
       num <- input$num
       dateRanges <- lapply(1:num, function(i) {
-        dateRangeInput(paste0("dates_", i), label = paste("Date Range", i), start = NULL, end = NULL)
+        numericRangeInput(paste0("dates_", i),
+                          label = paste("Year range", i),
+                          value = c(
+                            uploaded_data() %>%
+                              select(input$year) %>%
+                              summarise(min = min(eval(as.name(input$year)))) %>%
+                              pull(),
+                            uploaded_data() %>%
+                              select(input$year) %>%
+                              summarise(max = max(eval(as.name(input$year)))) %>%
+                              pull()
+                          )
+        )
       })
       tagList(dateRanges)
     })
 
-    output$species_id_plot <- renderPlot({
-      random_ggplot(type = "line")
-    })
+    observeEvent(input$plot_button, {
+      req(input$species, input$year, input$lon,
+          input$lat, input$ident, input$spat_uncert,
+          input$max_spat_uncert, input$type, uploaded_data())
 
+      dat <- as.data.frame(uploaded_data())
+
+      if (input$periodtype == "ranges") {
+        ranges_input_names <- paste0("dates_", 1:input$num)
+
+        # Filter the list based on 'period_ranges'
+        year_ranges <- lapply(ranges_input_names, function(x){ input[[x]]})
+
+        # Convert the year_ranges into vectors with year intervals of 1
+        periods <- lapply(year_ranges, function(element) {
+          seq(from = element[1], to = element[2])
+        })
+
+      } else{
+
+        periods = sort(unique(dat[[input$year]]))
+      }
+
+
+      output$species_id_plot <- renderPlot({
+
+        if (input$periodtype == "ranges") {
+
+          # Check for increasing years within each period
+          for(period in periods) {
+            validate(
+              need(min(period) == period[1] && max(period) == period[length(period)], "Period years are not in ascending order.")
+            )
+          }
+
+          # Check for overlapping periods
+          for(i in 1:(length(periods) - 1)) {
+            validate(
+              need(max(periods[[i]]) < min(periods[[i+1]]), "Period years are overlapping.")
+            )
+          }
+
+        }
+
+        assessSpeciesID(
+          dat = dat,
+          species = input$species,
+          periods = periods,
+          x = input$lon,
+          y = input$lat,
+          year = input$year,
+          spatialUncertainty = input$spat_uncert,
+          identifier = input$ident,
+          maxSpatUncertainty = input$max_spat_uncert,
+          type = input$type
+        )$plot
+      })
+    })
   })
 }
 
