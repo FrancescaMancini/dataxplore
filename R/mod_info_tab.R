aa79a129f5533b05cadc4957d12b2b3e94ad265b
FrancescaMancini <framan@ceh.ac.uk>
2025-02-14 09:15:25 +0000

    Add example dataset

--- R/mod_info_tab.R
+++ R/mod_info_tab.R
@@ -17,6 +17,7 @@ mod_info_tab_ui <- function(id){
                 uiOutput(ns("text1")),
                 h2("How to use dataXplore"),
                 uiOutput(ns("text2")),
+                tableOutput(ns("table")),
                 h2("Limitations and caveats"),
                 uiOutput(ns("text3")),
                 h2("Resources"),
@@ -45,13 +46,23 @@ mod_info_tab_server <- function(id){
     output$text2 <- renderUI({
       HTML(
         paste(
-          "<p>There are seven tabs in dataXplore. It is recommended that users read the information in the Info tab before attempting to use the app for the first time. After reading the Info tab, the user can upload the dataset they want to screen for bias in the “Data” tab. After the dataset has been uploaded, the user can open any of the other tabs in any order.</p>",
-          "<p>The app accepts data in csv format and the file must contain the following fields: species names; x and y coordinates of the species occurrence (any coordinate reference system may be used); alternatively British National grid References of the grid square in which the occurrence was recorded can be supplied and converted into latitude and longitude within the “Data” tab; the date of the occurrence; the year of the occurrence; the spatial uncertainty associated with the coordinates (units do not matter but must be consistent); and an identifier, which is used to split the data into groups (for example it could represent taxonomic groups or data sources). Missing information in any field should be indicated by “NA”. The column names do not need to match the fields, instead the column name for each field must be specified in the Data tab. See the table below for an example set of input data.</p>",
-          "<p>The tabs “Time”, “Species”, “Space” and “Environment” explore common biases in these domains. Each tab has a sidebar to specify arguments required by dataXplore to compute and visualise the metrics. After entering the required inputs, the user can click on the plot button to visualise the output.</p>",
+          "<p>There are seven tabs in dataXplore. It is recommended that users read the information in the Info tab before attempting to use the app for the first time. After reading the Info tab, the user can upload the dataset they want to screen for bias in the “Data” tab. After the dataset has been uploaded and all the required information entered in the “Data” tab, the user can open any of the other tabs in any order.</p>",
+          "<p>The app accepts data in csv format and the file must contain the following fields: species names; x and y coordinates of the species occurrence (any coordinate reference system may be used); alternatively British National grid References of the grid square in which the occurrence was recorded can be supplied and converted into latitude and longitude within the “Data” tab; the date of the occurrence; the year of the occurrence; the spatial uncertainty associated with the coordinates (units do not matter but must be consistent); and an identifier, which is used to split the data into groups (for example it could represent taxonomic groups or data sources). Missing information in any field should be indicated by “NA”. The column names do not need to match the fields, instead the column name for each field must be specified in the “Data” tab. See the table below for an example set of input data.</p>",
+          "<p>The tabs “Time”, “Species”, “Space” and “Environment” explore common biases in these domains. Each tab has a sidebar to specify arguments required by dataXplore to compute and visualise the metrics. After entering the required inputs, the user can click on the plot button to visualise the output. Each tab has guidance on the input required and what the output shows. You can download our worked examples to see what the outputs look like for simulated data.</p>",
           "<p>At the bottom of the sidebar in each tab, the user has the option to add the output, as well as the code and input used to generate it, to a downloadable report by ticking the checkbox “Add to report”.</p>"
         )
       )
     })
+
+    ex_tab <- data.frame(species = c("a", "b", "c", "d"),
+                         x = c(-4.629295227, -2.541747496, -0.080339407, -2.599618498),
+                         y = c(52.87884242, 51.74732335, 52.98631657, 52.30386493),
+                         date = c("27/10/2017", "30/01/2018", "01/07/2016", "03/06/2015"),
+                         year = c("2017", "2018", "2016", "2015"),
+                         group = c("A", "A", "C", "C"),
+                         sp_uncertainty = c("100", "10000", "1000", "100"))
+
+    output$table <- renderTable({ex_tab})
 
     output$text3 <- renderUI({
       HTML(
